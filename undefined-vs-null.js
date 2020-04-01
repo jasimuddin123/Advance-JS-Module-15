9bf92e2b74e3413f559a9ed5d46a6a0885ef38b7
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-01 13:52:32 -0700

    undefined -VS- Null Understand

---
+++ undefined-vs-null.js
@@ -0,0 +1,33 @@
+
+let pakhi;
+
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+
+
+const lover = {name:"Saima Akter", phone:438493};
+
+console.log(lover.boyFriend);
+
+
+
+
+const fun = "Undefined";
+
+console.log(fun);
+
+Below I have shown Null
+
+const ages = [22, 33, 55];
+
+console.log(ages[12]);
