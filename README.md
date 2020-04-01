16c0af8419f942f2810f229155371f2383ed6253
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-01 13:20:36 -0700

    Learning JS advance

---
+++ README.md
@@ -0,0 +1,1 @@
+# Advance-JS-Module-15
---
+++ trueAndFalse.js
@@ -0,0 +1,18 @@
+
+// null
+// undefined
+// 0
+// Falsy 
+// false
+// NAN
+
+const name ="Jasim Uddin";
+
+if(name.length > 0){
+
+    console.log("Condition is true");
+
+}
+else{
+    console.log("Condition is False");
+}
