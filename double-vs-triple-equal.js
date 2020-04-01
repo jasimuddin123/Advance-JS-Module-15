2a5c2d807136f1cc5c2563fcba7fcd2db9186b54
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-01 14:21:56 -0700

    double Vs triple Equal understan

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,41 @@
+const first = 2;
+const second = "2";
+
+if(first == second){
+    console.log(" Condition is true");
+}
+else{
+    console.log(" Condtion is False");
+}
+
+const age1 = 2;
+const age2 = "2";
+
+if(age1 === age2){
+    console.log(" condition is true");
+}
+
+else {
+    console.log("Condition is False");
+
+}
+    const ture = 1;
+    const true2 = true;
+    if(true == true2){
+        console.log("condition is true");
+    }
+else {
+    console.log("Condition is False");
+}
+
+// Note JavaScript true value count as a "1" and False value count as "0"
+
+const ture = 1;
+const true2 = true;
+if(true === true2){
+    console.log("condition is true");
+}
+else {
+console.log("Condition is False");
+
+// If we put === Equal it will show false due this should be same to same 
