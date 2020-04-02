dbcec31a0a706efd7467de51b36ded0b88e33465
jasimuddin123 <jasimuddindxn@gmail.com>
2020-04-01 17:39:40 -0700

    learn closure

---
+++ scope.js
@@ -0,0 +1,13 @@
+const bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    if(result < 9){
+        const mood ="happy";
+        console.log(mood);
+    }
+ return result;
+}
+
+// const output = sum(5, 1);
+// console.log(bonus);
+// console.log(output);
